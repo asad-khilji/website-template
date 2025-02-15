9a7544fdc86ba1cab28d9d28f617684612327e99
website-template <khiljiasad2@gmail.com>
2025-02-15 17:47:33 -0500

    added page outline

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const createHeader = () => {
+    let header = document.querySelector('header');
+
+    header.innerHTML = `
+   
+    `;
+}
+
+createHeader();
+
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+   
+    `;
+}
+
+createFooter();
